4bfe24ecdef28a57755abe7b68d9b5c02333494e
Josh Payne <paynejoshua1@gmail.com>
2021-12-22 07:17:03 -0800

    updated projects list

--- src/components/projectList/index.js
+++ src/components/projectList/index.js
@@ -1,9 +1,10 @@
 import Calendar from "../../assets/images/calendar.jpeg";
 import Password from "../../assets/images/password-generator.jpeg";
-import Mythos from "../../assets/images/mythosImage.png";
+// import Mythos from "../../assets/images/mythosImage.png";
 import Employee from "../../assets/images/EmployeeDirectoryImg.png";
 import YSMR from "../../assets/images/YSMR.png";
 import SunChaser from "../../assets/images/Sun Chaser.png";
+import CleverGame from "../../assets/images/CleverGame.png"
 
 
 const projectList =
@@ -22,6 +23,17 @@ const projectList =
     
   },
   {
+    "name": "Clever Game",
+    "repo": "https://github.com/paynejoshua/clevergame",
+    "deployed": "https://paynejoshua.github.io/clevergame",
+    "img": CleverGame,
+    "alt": "Clever Game",
+    "tools": "REACT, JavaScript, CSS, BootStrap, GitHub,",
+    "role": "Sole project coordinator and developer",
+    "note": "This is an ongoing project and will be updated regularly.",
+    "description": "A digital recreation of the dice game Ganz Schon (Thats so clever). This ongoing project is currently playable but stay tuned for updates as I work on bringing more complex elements to this project."
+  },
+  {
     "name": "Sun Chaser",
     "repo": "https://github.com/paynejoshua/findthesun",
     "deployed": "https://paynejoshua.github.io/findthesun/",
@@ -32,15 +44,15 @@ const projectList =
     "note": "This is an ongoing project and will be updated regularly.",
     "description": "Have you ever been affected by the lack of sun? Have you ever wondered where the sun just might be shining? With Sun Chaser you are never more than a click away from finding out where the sun is shining closest to you. No more gloomy blues! Chase the sun as you chase the blues away."
   },
-  {
-    "name": "Mythos",
-    "repo": "https://github.com/wkcodes/Mythos",
-    "deployed": "https://myth0s.herokuapp.com",
-    "img": Mythos,
-    "alt": "image for my project 2 website",
-    "tools": "Tools Used For this Project: HTML, CSS, React, Merge-Images, MySql, Node, Express, Heroku",
-    "role": "Developed and designed the Home, Portfolio, and World pages. Created the API routes to render information from our database to the portfolio page of the user who has signed in. Created the avatar maker on the portfolio page. Created the avatar characters and weapons and designed both so that a character and a weapon could be rendered on same image."
-  },
+  // {
+  //   "name": "Mythos",
+  //   "repo": "https://github.com/wkcodes/Mythos",
+  //   "deployed": "https://myth0s.herokuapp.com",
+  //   "img": Mythos,
+  //   "alt": "image for my project 2 website",
+  //   "tools": "Tools Used For this Project: HTML, CSS, React, Merge-Images, MySql, Node, Express, Heroku",
+  //   "role": "Developed and designed the Home, Portfolio, and World pages. Created the API routes to render information from our database to the portfolio page of the user who has signed in. Created the avatar maker on the portfolio page. Created the avatar characters and weapons and designed both so that a character and a weapon could be rendered on same image."
+  // },
   {
     "name": "Work Day Scheduler",
     "repo": "https://github.com/paynejoshua/Calendar",
